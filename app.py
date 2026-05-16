57519407ee555ccdc746cab251a1b6f96bf63048
RAJA V <rajavknm@gmail.com>
2026-05-16 12:25:08 +0200

    app.py

--- app.py
+++ app.py
@@ -3,32 +3,51 @@ from google import genai
 from google.genai import types
 from PIL import Image
 from io import BytesIO
+import os
 
-myrobo= genai.Client(api_key="#")
-col1,col2=st.columns(2)
+# API KEY
+myrobo = genai.Client(api_key="YOUR_API_KEY")
+
+col1, col2 = st.columns(2)
+
 with col1:
+
     st.title("IMAGE CREATOR")
-    userdata=st.text_area(" ")
+
+    userdata = st.text_area("Enter Prompt")
+
     if st.button("SEND"):
-        answer=myrobo.models.generate_content(
+
+        answer = myrobo.models.generate_content(
             model="gemini-2.0-flash-preview-image-generation",
             contents=userdata,
             config=types.GenerateContentConfig(
-            response_modalities=['TEXT','IMAGE']
+                response_modalities=['TEXT', 'IMAGE']
             )
         )
+
         for part in answer.candidates[0].content.parts:
+
             if part.text is not None:
-                print(part.text)
+                st.write(part.text)
+
             elif part.inline_data is not None:
-                image=Image.open(BytesIO((part.inline_data.data)))
+
+                image = Image.open(BytesIO(part.inline_data.data))
+
                 image.save("Myimage.png")
 
+                st.session_state["img_created"] = True
+
+
 with col2:
-    if st.image("Myimage.png"):
-        st.write("Your Image Has Been Created Successfully")
+
+    if os.path.exists("Myimage.png"):
+
+        st.image("Myimage.png")
+
+        st.success("Your Image Has Been Created Successfully")
 
     else:
-        st.write("Waiting for Your Response......")
-       
-             
+
+        st.write("Waiting for Your Response.....")
